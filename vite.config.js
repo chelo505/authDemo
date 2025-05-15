1677f45704a9f294eb4480202ad40cce9db83266
chelo505 <datobaqro@gmail.com>
2025-05-15 21:58:54 +0400

    ghpages

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/testProj/',  // Replace with your repository name
   server: {
     port: 5173,
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false
+        changeOrigin: true
       }
     }
   }
